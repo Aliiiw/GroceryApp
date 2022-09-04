3a2936252b6f3ec3b6f59bf2e18a6642c69310fa
Ali Rahimi <1aliirahimi1@gmail.com>
2022-09-05 00:04:59 +0430

    adding header to app bar

--- app/src/main/java/com/alirahimi/groceryapp/screens/Content.kt
+++ app/src/main/java/com/alirahimi/groceryapp/screens/Content.kt
@@ -1,9 +1,15 @@
 package com.alirahimi.groceryapp.screens
 
+import android.preference.PreferenceActivity
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun Content(paddingValues: PaddingValues){
-
+    Column(modifier = Modifier.padding(paddingValues)) {
+        Header()
+    }
 }
---
+++ app/src/main/java/com/alirahimi/groceryapp/screens/Header.kt
@@ -0,0 +1,77 @@
+package com.alirahimi.groceryapp.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.alirahimi.groceryapp.R
+
+@Composable
+fun Header() {
+    Card(
+        modifier = Modifier
+            .height(64.dp)
+            .padding(horizontal = 16.dp),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            QrButton()
+
+            VerticalDivider()
+
+            Row(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .clickable { }
+                    .padding(horizontal = 8.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_money),
+                    contentDescription = "",
+                    tint = Color(0xFF6FCF97)
+                )
+                Column(modifier = Modifier.padding(8.dp)) {
+                    Text(text = "$130", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                    Text(text = "Top Up", color = MaterialTheme.colors.primary, fontSize = 12.sp)
+                }
+            }
+
+            VerticalDivider()
+
+            Row(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .clickable { }
+                    .padding(horizontal = 8.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_coin),
+                    contentDescription = "",
+                    tint = MaterialTheme.colors.primary
+                )
+                Column(modifier = Modifier.padding(8.dp)) {
+                    Text(text = "20", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                    Text(text = "Points", color = Color.LightGray, fontSize = 12.sp)
+                }
+            }
+        }
+    }
+}
