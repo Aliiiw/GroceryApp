e736b82484f4c72cd7b5e88302d3639b1d79e4bd
Ali Rahimi <1aliirahimi1@gmail.com>
2022-09-04 23:43:51 +0430

    create app bar

---
+++ app/src/main/java/com/alirahimi/groceryapp/screens/AppBar.kt
@@ -0,0 +1,51 @@
+package com.alirahimi.groceryapp.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AppBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(text = "Search food, vegetable and ...", fontSize = 16.sp)
+            },
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = ""
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        )
+    }
+}
--- app/src/main/java/com/alirahimi/groceryapp/screens/HomeScreen.kt
+++ app/src/main/java/com/alirahimi/groceryapp/screens/HomeScreen.kt
@@ -1,8 +1,42 @@
 package com.alirahimi.groceryapp.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.alirahimi.groceryapp.R
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
+    Box() {
+
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(0.dp, (-30).dp),
+            painter = painterResource(id = R.drawable.bg_main),
+            contentDescription = "",
+            contentScale = ContentScale.FillWidth
+        )
+
+        Scaffold(
+            topBar = {AppBar()},
+            backgroundColor = Color.Transparent
+        ){
+            paddingValues ->
+            Content(paddingValues)
+        }
+
+
+
+    }
+
 
 }
